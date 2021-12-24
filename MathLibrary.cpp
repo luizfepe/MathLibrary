9cfc92c060d9cec10cd70895fd244bf4f9de0c04
luizfepe luizfepe <luizfepe@gmail.com>
2021-12-24 19:17:53 +0900

    Project finalized. However C# applies wrong orientation to Avatar.

--- MathLibrary.cpp
+++ MathLibrary.cpp
@@ -9,63 +9,235 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <k4a/k4a.h>
-#include <k4abt.h>
+//#include <k4a/k4a.h>
+//#include <k4abt.h>
 //#include "packages/Microsoft.Azure.Kinect.Sensor.1.3.0/build/native/include/k4a/k4a.h"
 //#include "packages/Microsoft.Azure.Kinect.BodyTracking.1.0.0/build/native/include/k4abt.h"
 
+using namespace std;
+#define VERIFY(result, error)                                                                            \
+    if(result != K4A_RESULT_SUCCEEDED)                                                                   \
+    {                                                                                                    \
+        printf("%s \n - (File: %s, Function: %s, Line: %d)\n", error, __FILE__, __FUNCTION__, __LINE__); \
+        exit(1);                                                                                         \
+    }          
 
 // DLL internal state variables:
 static unsigned long long previous_;  // Previous value, if any
 static unsigned long long current_;   // Current sequence value
 static unsigned index_;               // Current seq. position
 
-// Initialize a Fibonacci relation sequence
-// such that F(0) = a, F(1) = b.
-// This function must be called before any other function.
-void fibonacci_init(
-    const unsigned long long a,
-    const unsigned long long b)
+
+//KINECT
+static k4a_device_t device;
+static k4abt_tracker_t tracker = NULL;
+static k4abt_body_t* bodyToReturn = NULL;
+
+void KinectInit() 
 {
-    index_ = 0;
-    current_ = a;
-    previous_ = b; // see special case when initialized
+    //k4a_device_t device;
+    //k4abt_tracker_t tracker = NULL;
+    k4a_device_configuration_t device_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+    device_config.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
+
+    //k4a_device_open(0, &device);
+    VERIFY(k4a_device_open(0, &device), "Open K4A Device failed!");
+    VERIFY(k4a_device_start_cameras(device, &device_config), "Start K4A cameras failed!");
+
+    k4a_calibration_t sensor_calibration;
+    VERIFY(k4a_device_get_calibration(device, device_config.depth_mode, K4A_COLOR_RESOLUTION_OFF, &sensor_calibration),
+        "Get depth camera calibration failed!");
+
+
+    k4abt_tracker_configuration_t tracker_config = K4ABT_TRACKER_CONFIG_DEFAULT;
+    VERIFY(k4abt_tracker_create(&sensor_calibration, tracker_config, &tracker), "Body tracker initialization failed!");
 }
 
-// Produce the next value in the sequence.
-// Returns true on success, false on overflow.
-bool fibonacci_next()
+void KinectFinish()
 {
-    // check to see if we'd overflow result or position
-    if ((ULLONG_MAX - previous_ < current_) ||
-        (UINT_MAX == index_))
+    k4abt_tracker_shutdown(tracker);
+    k4abt_tracker_destroy(tracker);
+    k4a_device_stop_cameras(device);
+    k4a_device_close(device);
+}
+
+
+uint32_t GetNumBodies() {
+    int frame_count = 0;
+    uint32_t numbodiess = 0;
+    do
     {
-        return false;
-    }
+        k4a_capture_t sensor_capture;
+        k4a_wait_result_t get_capture_result = k4a_device_get_capture(device, &sensor_capture, K4A_WAIT_INFINITE);
+        if (get_capture_result == K4A_WAIT_RESULT_SUCCEEDED)
+        {
+            frame_count++;
 
-    // Special case when index == 0, just return b value
-    if (index_ > 0)
+            printf("Start processing frame %d\n", frame_count);
+
+            k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(tracker, sensor_capture, K4A_WAIT_INFINITE);
+
+            k4a_capture_release(sensor_capture);
+            if (queue_capture_result == K4A_WAIT_RESULT_TIMEOUT)
+            {
+                // It should never hit timeout when K4A_WAIT_INFINITE is set.
+                printf("Error! Add capture to tracker process queue timeout!\n");
+                break;
+            }
+            else if (queue_capture_result == K4A_WAIT_RESULT_FAILED)
+            {
+                printf("Error! Add capture to tracker process queue failed!\n");
+                break;
+            }
+
+            k4abt_frame_t body_frame = NULL;
+            k4a_wait_result_t pop_frame_result = k4abt_tracker_pop_result(tracker, &body_frame, K4A_WAIT_INFINITE);
+            if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
+            {
+                uint32_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
+                numbodiess = num_bodies ;
+                printf("%u bodies are detected!\n", num_bodies);
+
+                for (uint32_t i = 0; i < num_bodies; i++)
+                {
+                    k4abt_body_t body;
+                    VERIFY(k4abt_frame_get_body_skeleton(body_frame, i, &body.skeleton), "Get body from body frame failed!");
+                    body.id = k4abt_frame_get_body_id(body_frame, i);
+
+                    //print_body_information(body);
+                }
+
+                k4a_image_t body_index_map = k4abt_frame_get_body_index_map(body_frame);
+                if (body_index_map != NULL)
+                {
+                    //print_body_index_map_middle_line(body_index_map);
+                    k4a_image_release(body_index_map);
+                }
+                else
+                {
+                    printf("Error: Fail to generate bodyindex map!\n");
+                }
+
+                k4abt_frame_release(body_frame);
+            }
+            else if (pop_frame_result == K4A_WAIT_RESULT_TIMEOUT)
+            {
+                //  It should never hit timeout when K4A_WAIT_INFINITE is set.
+                printf("Error! Pop body frame result timeout!\n");
+                break;
+            }
+            else
+            {
+                printf("Pop body frame result failed!\n");
+                break;
+            }
+        }
+        else if (get_capture_result == K4A_WAIT_RESULT_TIMEOUT)
+        {
+            // It should never hit time out when K4A_WAIT_INFINITE is set.
+            printf("Error! Get depth frame time out!\n");
+            break;
+        }
+        else
+        {
+            printf("Get depth capture returned error: %d\n", get_capture_result);
+            break;
+        }
+
+    } while (false);
+    return numbodiess;
+}
+
+k4abt_body_t* CaptureFrame()
+{
+    bodyToReturn = NULL;
+    bool bodyReady = false;
+    k4a_capture_t sensor_capture;
+    k4a_wait_result_t get_capture_result = k4a_device_get_capture(device, &sensor_capture, K4A_WAIT_INFINITE);
+    if (get_capture_result == K4A_WAIT_RESULT_SUCCEEDED)
     {
-        // otherwise, calculate next sequence value
-        previous_ += current_;
+        //frame_count++;
+        //printf("Start processing frame %d\n", frame_count);
+
+        k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(tracker, sensor_capture, K4A_WAIT_INFINITE);
+
+        k4a_capture_release(sensor_capture);
+        if (queue_capture_result == K4A_WAIT_RESULT_TIMEOUT)
+        {
+            // It should never hit timeout when K4A_WAIT_INFINITE is set.
+            //printf("Error! Add capture to tracker process queue timeout!\n");
+            //break;
+        }
+        else if (queue_capture_result == K4A_WAIT_RESULT_FAILED)
+        {
+            //printf("Error! Add capture to tracker process queue failed!\n");
+            //break;
+        }
+
+        k4abt_frame_t body_frame = NULL;
+        k4a_wait_result_t pop_frame_result = k4abt_tracker_pop_result(tracker, &body_frame, K4A_WAIT_INFINITE);
+        if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
+        {
+            uint32_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
+            //printf("%u bodies are detected!\n", num_bodies);
+
+            for (uint32_t i = 0; i < num_bodies; i++)
+            {
+                k4abt_body_t body;
+                VERIFY(k4abt_frame_get_body_skeleton(body_frame, i, &body.skeleton), "Get body from body frame failed!");
+                body.id = k4abt_frame_get_body_id(body_frame, i);
+                if (!bodyReady)
+                {
+                    bodyReady = true;
+                    bodyToReturn = new k4abt_body_t;
+                    *bodyToReturn = body;
+                }
+                //print_body_information(body);
+            }
+
+            k4a_image_t body_index_map = k4abt_frame_get_body_index_map(body_frame);
+            if (body_index_map != NULL)
+            {
+                //print_body_index_map_middle_line(body_index_map);
+                k4a_image_release(body_index_map);
+            }
+            else
+            {
+                //printf("Error: Fail to generate bodyindex map!\n");
+            }
+
+            k4abt_frame_release(body_frame);
+        }
+        else if (pop_frame_result == K4A_WAIT_RESULT_TIMEOUT)
+        {
+            //  It should never hit timeout when K4A_WAIT_INFINITE is set.
+            //printf("Error! Pop body frame result timeout!\n");
+            //break;
+        }
+        else
+        {
+            //printf("Pop body frame result failed!\n");
+            //break;
+        }
     }
-    std::swap(current_, previous_);
-    ++index_;
-    return true;
+    //else if (get_capture_result == K4A_WAIT_RESULT_TIMEOUT)
+    //{
+    //    // It should never hit time out when K4A_WAIT_INFINITE is set.
+    //    //printf("Error! Get depth frame time out!\n");
+    //    //break;
+    //}
+    //else
+    //{
+    //    //printf("Get depth capture returned error: %d\n", get_capture_result);
+    //    //break;
+    //}
+    return bodyToReturn;
 }
 
-// Get the current value in the sequence.
-unsigned long long fibonacci_current()
-{
-    return current_;
-}
 
-// Get the current index position in the sequence.
-unsigned fibonacci_index()
-{
-    return index_;
-}
-int retornateste()
-{
-    return 25;
+Teste* Testa() {
+    Teste* Thiago = new Teste;
+    Thiago->t[0] = 5;
+    Thiago->t[1] = 6;
+    return Thiago;
 }
--- MathLibrary.h
+++ MathLibrary.h
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+#include <k4a/k4a.h>
+#include <k4abt.h>
 
 #ifdef MATHLIBRARY_EXPORTS
 #define MATHLIBRARY_API __declspec(dllexport)
@@ -12,34 +14,19 @@ extern "C" {
 #define MATHLIBRARY_API __declspec(dllimport)
 #endif
 
-// The Fibonacci recurrence relation describes a sequence F
-// where F(n) is { n = 0, a
-//               { n = 1, b
-//               { n > 1, F(n-2) + F(n-1)
-// for some initial integral values a and b.
-// If the sequence is initialized F(0) = 1, F(1) = 1,
-// then this relation produces the well-known Fibonacci
-// sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
-
-// Initialize a Fibonacci relation sequence
-// such that F(0) = a, F(1) = b.
-// This function must be called before any other function.
-extern "C" MATHLIBRARY_API void fibonacci_init(
-    const unsigned long long a, const unsigned long long b);
-
-// Produce the next value in the sequence.
-// Returns true on success and updates current value and index;
-// false on overflow, leaves current value and index unchanged.
-extern "C" MATHLIBRARY_API bool fibonacci_next();
-
-// Get the current value in the sequence.
-extern "C" MATHLIBRARY_API unsigned long long fibonacci_current();
-
-// Get the position of the current value in the sequence.
-extern "C" MATHLIBRARY_API unsigned fibonacci_index();
+    struct Teste
+    {
+        int t[2] = {1, 2};
+    };
 
 extern "C" MATHLIBRARY_API int retornateste();
 
+extern "C" MATHLIBRARY_API void KinectInit();
+extern "C" MATHLIBRARY_API uint32_t GetNumBodies();
+
+extern "C" MATHLIBRARY_API void KinectFinish();
+extern "C" MATHLIBRARY_API k4abt_body_t* CaptureFrame();
+extern "C" MATHLIBRARY_API Teste* Testa();
 #ifdef __cplusplus
 }
 #endif
